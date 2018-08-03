6cc754f2fcf89245e6b1cc4e64da26274e720046
Admin <Barrya42@gmail.com>
2018-08-04 00:06:35 +0700

    Requests is works!!! all works!!!

--- Door.cpp
+++ Door.cpp
@@ -1,133 +1,164 @@
-#define RespLength 2
 #include <SoftwareSerial.h>
 #include <Arduino.h>
 
-
-
 SoftwareSerial BTSerial(2, 3); // указываем пины RX и TX
-int pinReley = 4;
-byte helloFromPhone[] = {0x02, 0x01};
-byte OpenDoorCommand[] = {0x02, 0x05};
-byte Helloed[] = {0x0E, 0x00};
-byte Hello[] = {0x0E, 0x01};
-byte NotYetHelloed[RespLength] = {0x0E, 0x02};
+int pinReley = 13;
+int pinDeviceConnected = 4;
+bool DEBUG = true;
+
+const String REQUEST_OPEN_DOOR = "54141f5a-fb34-494e-a9b4-516689876310";
+const String REQUEST_HELLO = "3dfe191f-15f2-43cf-b2ac-4ac6d1c928fc";
+const String REQUEST_CLOSE_DOOR = "c6361166-4c22-4bd9-b682-49812110f605";
+String Ok = "200:OK";
+String Helloed = "501:Helloed";
+String NotYetHelloed = "501:NotHelloed";
 bool isHelloed = false;
+
 long openMoment;
-byte request[RespLength];
 long openDelay = 6000;
-void ReciveRequest()
+
+void PrintDebug(String message)
 {
-    while(BTSerial.available())
-    {
-      Serial.write(BTSerial.read());
-    }
-  //String str = BTSerial.readString();
-  BTSerial.readBytes(request, RespLength);
-  Serial.write(request,RespLength);
-
-  //BTSerial.flush();
-
+	if (DEBUG)
+		Serial.println(message);
 }
 
-
-
-void sendHello()
+String ReciveRequest()
 {
-  BTSerial.write(Hello,RespLength);
+	String res = "";
+	if (BTSerial.available())
+	{
+		res = BTSerial.readString();
 
+		PrintDebug("recive request: " + res);
+	}
+	return res;
 }
 
+//bool CompareArrays(byte* response, byte* tamplate)
+//{
+//	for (int i = 0; i <= RespLength - 1; i++)
+//	{
+//		if (response[i] != tamplate[i])
+//			return false;
+//	}
+//	return true;
+//}
 
-void sendHelloed()
+void CloseDoorByTimer(bool force)
 {
-  BTSerial.write(Helloed,RespLength);
+	if (millis() - openMoment >= openDelay || force)
+	{
+		digitalWrite(pinReley, LOW);
+	}
 }
 
-
-void sendNotHello()
+void OpenDoor()
 {
-  BTSerial.write(NotYetHelloed,RespLength);
+	digitalWrite(pinReley, HIGH);
 }
 
-
-bool CompareArrays(byte* response, byte* tamplate)
+void CheckConnected()
 {
-  for(int i =0;i<=RespLength-1;i++)
-  {
-    if(response[i] != tamplate[i])
-    return false;
-  }
-  return true;
+	if(digitalRead(pinDeviceConnected) == LOW && isHelloed)
+	{
+		isHelloed = false;
+		PrintDebug("device DISCONNECTED, ""isHelloed = false""");
+	}
+	if(digitalRead(pinDeviceConnected) == HIGH)
+	{
+		PrintDebug("device CONNECTED");
+	}
 }
 
-
+void SendResponse(String resp)
+{
+	BTSerial.print(resp);
+}
 void setup()
 {
-  pinMode(pinReley,OUTPUT);
-  digitalWrite(pinReley,LOW);
-  pinMode(13, OUTPUT);
+	pinMode(pinReley, OUTPUT);
+	digitalWrite(pinReley, LOW);
+	pinMode(pinDeviceConnected,INPUT);
 //pinMode(2,INPUT);
 //pinMode(3,OUTPUT);
-Serial.begin(9600);
-BTSerial.begin(9600);
+	Serial.begin(9600);
+	BTSerial.begin(38400);
 }
-
 
 void loop()
 {
-  if(millis()-openMoment >= openDelay)
-  {
-    digitalWrite(pinReley,LOW);
-  }
-  if (BTSerial.available())
-  {
-    if(!isHelloed)
-    {
-    	ReciveRequest();
-      if(CompareArrays(request, helloFromPhone))
-      //if("11" == "11")
-      {
-        sendHello();
-        isHelloed = true;
-      }
-      else
-      {
-        sendNotHello();
-      }
-    }
-    else
-    {
-      sendHelloed();
-    }
-    ReciveRequest(); // читаем из software-порта
-    if(CompareArrays(request, OpenDoorCommand ))
-    {
-      if(isHelloed)
-      {
-        digitalWrite(pinReley,HIGH);
-        openMoment = millis();
-      }
-      else
-      {
-        sendNotHello();
-      }
-    }
-   Serial.write(request,RespLength);
-  }
+	CloseDoorByTimer(false);
+	CheckConnected();
+
+	String res = ReciveRequest();
+
+	if (res.equals(""))
+	{
+		//PrintDebug("empty request");
+	}
+	else if (res.equals(REQUEST_HELLO))
+	{
+		//телефон пробует поздороваться
+		if (isHelloed)
+		{
+			PrintDebug("Hello request and we are HELLOED");
+			SendResponse(Helloed);
+		}
+		else
+		{
+			PrintDebug("Hello request and we are NOT HELLOED");
+			isHelloed = true;
+			SendResponse(Ok);
+		}
+	}
+	else if (res.equals(REQUEST_CLOSE_DOOR))
+	{
+		//Телефон рпосит открыть дверь
+		if (isHelloed)
+		{
+			PrintDebug("Close request and we HELLOED");
+			CloseDoorByTimer(true);
+			SendResponse(Ok);
+		}
+		else
+		{
+			PrintDebug("Close request and we are NOT HELLOED");
+			SendResponse(NotYetHelloed);
+		}
+	}
+	else if (res.equals(REQUEST_OPEN_DOOR))
+	{
+		if (isHelloed)
+		{
+			PrintDebug("Open request and we are HELLOED");
+			OpenDoor();
+			SendResponse(Ok);
+		}
+		else
+		{
+			PrintDebug("Open request and we are NOT HELLOED");
+			SendResponse(NotYetHelloed);
+		}
+	}
+	else
+	{
+		PrintDebug("Unknown command: " + res);
+		SendResponse("500:Unknown command");
+	}
 
 //Если вдруг с PC команду нужно отправить
-if (Serial.available())
-{
-  int pcData = Serial.read(); // читаем из hardware-порта
-  BTSerial.println(pcData); // пишем в software-порт
+	if (Serial.available())
+	{
+		BTSerial.println(Serial.readString()); // пишем в software-порт
 
+	}
 }
 
 //логи
 //Serial.println(request); // пишем в hardware-порт
-}
 
- //8C3A:E3:ED8235
+//8C3A:E3:ED8235
 //18F0:E4:2F8153
 
 //0018:e4:34bf20
